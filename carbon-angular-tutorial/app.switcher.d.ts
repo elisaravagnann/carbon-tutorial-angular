405795417b53ebcf850436a822b57f76fddc8c95
elisaravagnann <elisa.ravagnan@beantech.it>
2024-02-21 14:13:01 +0100

    feat(tutorial): complete step 1

---
+++ carbon-angular-tutorial/app.switcher.d.ts
@@ -0,0 +1,4 @@
+declare module '@carbon/icons/es/app-switcher/20' {
+    const AppSwitcher20: any;
+    export default AppSwitcher20;
+  }
---
+++ carbon-angular-tutorial/src/typings/user-avatar.d.ts
@@ -0,0 +1,4 @@
+declare module '@carbon/icons/es/user--avatar/20' {
+    const UserAvatar20: any;
+    export default UserAvatar20;
+  }
